3316b683d0e5cd7b8736ebc5f7bb2180d6b3b223
Troy Dieter <tadieter@amazon.com>
2025-01-29 14:15:37 -0500

    dropped zone var

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
 variable "aws_region" {
   type        = string
   description = "AWS Region"
-  default     = "us-east-1"
+  default     = "us-east-2"
 }
 variable "vpc" {
   type        = string
   description = "VPC to deploy to"
-  default     = "vpc-0a2a83d4e068c74c6"
+  default     = "vpc-028a6a7484d0cafce"
 }
 
 variable "home_ip" {
@@ -18,7 +18,7 @@ variable "home_ip" {
 variable "ami" {
   type        = string
   description = "AMI to be used"
-  default     = "ami-0e1bed4f06a3b463d"
+  default     = "ami-0884d2865dbe9de4b"
 }
 
 variable "environment" {
@@ -31,12 +31,6 @@ variable "instance_size" {
   type        = string
   description = "The instance size"
   default     = "g4dn.xlarge"
-}
-
-variable "zone_id" {
-  type        = string
-  description = "Route53 Zone ID"
-  default     = "Z10057183MLGVY8QQ2VGK"
 }
 
 variable "key_name" {
